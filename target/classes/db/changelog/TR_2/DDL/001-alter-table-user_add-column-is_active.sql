0908b2ffd46ba738c35a46b44484999b5f74401f
serg_phom <group35student@gmail.com>
2026-03-30 09:43:14 +0300

    add rm target/

--- target/classes/db/changelog/TR_2/DDL/001-alter-table-user_add-column-is_active.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
-
---changeset serg_phom:alter-table-bank_user-add-column-is_active
-alter table bank_user add column if not exists is_active boolean default true;
---rollback alter table bank_user drop column if exists is_active;
